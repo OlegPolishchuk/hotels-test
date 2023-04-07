bb0e3655a5f66891faa3261f8f4260c95073e3eb
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-04-07 20:05:51 +0300

    fix: remove autoplay from carousel

--- src/components/carousel/CustomSlider.tsx
+++ src/components/carousel/CustomSlider.tsx
@@ -1,19 +1,13 @@
 import React, { ReactElement } from 'react';
 
-import Carousel, { ScrollMode } from 'nuka-carousel';
+import Carousel from 'nuka-carousel';
 
 import styles from './Carousel.module.css';
 
 export const CustomSlider = (): ReactElement => {
   return (
     <div className={styles.slider}>
-      <Carousel
-        autoplay
-        wrapAround
-        slidesToShow={3}
-        scrollMode={'remainder' as ScrollMode}
-        withoutControls
-      >
+      <Carousel slidesToShow={3} renderBottomCenterControls={() => null}>
         <img src="/carousel/slide_1.jpg" alt="slider 1" />
         <img src="/carousel/slide_2.jpg" alt="slider 2" />
         <img src="/carousel/slide_3.jpg" alt="slider 3" />
